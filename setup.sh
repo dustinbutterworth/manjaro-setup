da6dd90b9ccacd5c85f763325640f250c9c56ccb
Dustin Butterworth <51181391+dustinbutterworth@users.noreply.github.com>
2021-11-06 13:36:46 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
+chsh -s $(which zsh)
 
 
 print "pyenv setup - bash"
